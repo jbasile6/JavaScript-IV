b02cce7bfc1d7b319062d599a441ba5a37915759
James Basile <jbasile14@gmail.com>
2019-01-24 16:41:12 -0500

    Person and Instructor constructor class/methods built

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,41 @@
 // CODE here for your Lambda Classes
+
+//Person class constructor
+class Person {
+    constructor(personProps) {
+        this.name = personProps.name;
+        this.age = personProps.age;
+        this.location = personProps.location;
+        this.gender = personProps.gender;
+    }
+    //Person Methods
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
+    }
+}
+
+/*--------------------------children of Person-----------------------------------------*/
+
+//Instructor class constructor
+class Instructor extends Person {
+    constructor(instructorProps) {
+        super(instructorProps);
+        this.specialty = instructorProps.specialty;
+        this.favLanguage = instructorProps.favLanguage;
+        this.catchPhrase = instructorProps.catchPhrase;
+    }
+    //Instructor Methods
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(studentName, subject) {
+        console.log(`${studentName} receives a perfect score on ${subject}`);
+    }
+}
+
+
+//Student class constructor
+
+
+
+/*--------------------------children of Person END-----------------------------------------*/
